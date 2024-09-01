f279f06162e6770c7076972227cdd6315a8c4375
Fredyxsen <diegomarinm130@gmail.com>
2024-09-01 18:13:43 -0400

    Mi mensaje de commits

--- user/yosoytupadre.c
+++ user/yosoytupadre.c
@@ -2,31 +2,16 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int getancestor(int n) {
-    if (n < 0) {
-        // Nivel de ancestro inválido
-        return -1;
-    }
-
-    int pid = getpid();
-    for (int i = 0; i < n; ++i) {
-        pid = getppid();
-        if (pid == 1) { // Si llegamos al proceso init, no hay más ancestros
-            return -1;
-        }
-    }
-    return pid;
-}
-
 int main(void) {
+    int ppid = getppid();
+    printf("El ProcessID del proceso padre es: %d\n", ppid);
+
     int n = 2; // Valor según ancestro que se quiera obtener
     int ancestor_pid = getancestor(n);
-
-    if (ancestor_pid != -1) {
+    if (ancestor_pid != -1)
         printf("El ProcessID del ancestro %d es: %d\n", n, ancestor_pid);
-    } else {
+    else
         printf("No se encontró un ancestro en el nivel %d.\n", n);
-    }
 
     exit(0);
 }
