466e6d6429944842831b38c8a86e75ad7ab6d2c6
Fredyxsen <diegomarinm130@gmail.com>
2024-09-01 18:25:35 -0400

    Mi mensaje de commitsass

--- user/yosoytupadre.c
+++ user/yosoytupadre.c
@@ -1,21 +1,22 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+
 int main() {
-    // Obtenemos el PID del proceso padre
-    int proceso_padre = getppid();
-    printf("El ID del proceso padre es: %d\n", proceso_padre);
+    // Obtener el PID del proceso padre
+    int pid_padre = getppid();
+    printf("El PID del proceso padre es: %d\n", pid_padre);
 
-    // Buscamos el ancestro en el segundo nivel
-    int nivel_ancestro = 2;
-    int id_ancestro = encontrar_ancestro(nivel_ancestro);
+    // Definir el nivel del ancestro a buscar
+    const int nivel_ancestro = 2;
+    int pid_ancestro = encontrar_ancestro(nivel_ancestro);
 
-    // Imprimimos el resultado
-    if (id_ancestro != -1) {
-        printf("El ancestro en el nivel %d es: %d\n", nivel_ancestro, id_ancestro);
+    // Mostrar el resultado
+    if (pid_ancestro != -1) {
+        printf("El PID del ancestro en el nivel %d es: %d\n", nivel_ancestro, pid_ancestro);
     } else {
         printf("No se encontró un ancestro en el nivel %d.\n", nivel_ancestro);
     }
 
-    return 0;  // Equivalente a exit(0)
+    return 0;  // Finaliza el programa correctamente
 }
