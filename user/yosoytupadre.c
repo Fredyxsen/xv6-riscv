659a5cfb62e85b7cfb63c9ee68e8b5903533f60a
Fredyxsen <diegomarinm130@gmail.com>
2024-09-01 18:37:58 -0400

    Mi mensaje de commitsas321123133s255

--- user/yosoytupadre.c
+++ user/yosoytupadre.c
@@ -4,12 +4,12 @@
 
 int main(void) {
     int ppid = getppid();
-    printf("El ProcessID del proceso padre es: %d\n", ppid);
+    printf("El ID de proceso del proceso padre es: %d\n", ppid);
 
-    int n = 2; // Valor según ancestro que se quiera obtener
+    int n = 2; // Valor indicado según el ancestro que se quiera ver
     int ancestor_pid = getancestor(n);
     if (ancestor_pid != -1)
-        printf("El ProcessID del ancestro %d es: %d\n", n, ancestor_pid);
+        printf("El id del proceso que creó el proceso que está ejecutando esta función %d es: %d\n", n, ancestor_pid);
     else
         printf("No se encontró un ancestro en el nivel %d.\n", n);
 
